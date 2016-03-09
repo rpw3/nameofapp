01cd5d334d40e36570123fe2529d2ef1e0dff322
Bob Wright <bwright13@hotmail.com>
2016-03-09 14:49:21 -0700

    added images to carousel

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,13 @@
 class StaticPagesController < ApplicationController
+  
   def landing_page
   	@featured_product = Product.first
   end
+
+  def landing_page
+  	@products = Product.limit(3)
+  end	
+
 end
+
+
