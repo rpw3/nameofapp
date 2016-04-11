4025ff078d1d7b9e01342402dcb4b3393586b9de
Bob Wright <bwright13@hotmail.com>
2016-04-10 18:12:43 -0600

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -27,17 +27,9 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+ resources :products do
+    resources :comments
+ end   
 
   # Example resource route with sub-resources:
   #   resources :products do
