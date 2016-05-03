ca3aa0d41684a934f1276507b9e280afc54ee1d5
Bob Wright <bwright13@hotmail.com>
2016-05-02 20:36:43 -0600

    added functional tests to spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,23 +3,56 @@ require 'rails_helper'
 describe UsersController, :type => :controller do
 
   before do
-    @user = User.create!(email: "peter@example.com", password: "1234567890")
+    @userA = User.create!(email: "peter@example.com", password: "1234567890")
+    @userB = User.create!(email: "bob@example.com", password: "1234567890")
   end
 
 
   describe "GET #show" do
     context "User is logged in" do
       before do
-        sign_in @user
+        sign_in @userA
       end
 
       it "loads correct user details" do
-        get :show, id: @user.id
+        get :show, id: @userA.id 
         expect(response).to have_http_status(200)
-        expect(assigns(:user)).to eq @user
+        expect(assigns(:user)).to eq @userA
       end
     end
     
- 
+    context "No user is logged in" do
+      it "redirects to login" do
+        get :show, id: @userA.id
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
   end
+
+
+    describe "GET #show" do
+    context "User is logged in" do
+      before do
+        sign_in @userB
+      end
+
+      it "loads correct user details" do
+        get :show, id: @userB.id 
+        expect(response).to have_http_status(200)
+        expect(assigns(:user)).to eq @userB
+      end
+    end
+    
+    context "No user is logged in" do
+      it "redirects to login" do
+        get :show, id: @userB.id
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+
+
+
+
 end
